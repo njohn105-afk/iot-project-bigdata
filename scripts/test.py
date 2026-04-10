f3dd152caf3c0f9d7eb279bd51d2706fc483e77e
njohn105-afk <njohn105@kent.edu>
2026-04-09 21:24:06 -0400

    asd

--- scripts/test.py
+++ scripts/test.py
@@ -1,15 +1,12 @@
 from scapy.all import PcapReader
 from scapy.layers.inet import IP, TCP
 
-print("starting")
 
 count = 0
 
 # Other packet fields that may be useful later
-
 # packet.time -> timestamp (unix) | useful for building time windows
 # len(packet) -> packet length | total packet size in bytes
-
 # Todo: output to csv
 
 
@@ -17,16 +14,8 @@ count = 0
 with PcapReader("data/day1_only.pcap") as packets:
     for packet in packets:
         count+=1
-        if packet.haslayer(IP):
-            src_ip = packet[IP].src
-            dst_ip = packet[IP].dst
-            print(f"Source IP: {src_ip}, Destination IP: {dst_ip}")
-
-        if packet.haslayer(TCP):
-            src_port = packet[TCP].sport
-            dst_port = packet[TCP].dport
-            print(f"TCP Packet - Source Port: {src_port}, Destination Port: {dst_port}")
+        print(packet);
         if count == 10:
-            break 
+            break
 
-print("Completed 1000 packets")
+print("Test complete")
